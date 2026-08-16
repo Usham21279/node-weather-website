6dfec8830e21e043dedf7dfe5da0ad3a9401e15a
Usham21279 <usha21279@gmail.com>
2026-08-16 18:51:12 +0530

    Initial upload of project files

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+const weatherForm = document.querySelector("form");
+const serach = document.querySelector("input");
+const msg1 = document.getElementById("msg-1");
+const msg2 = document.getElementById("msg-2");
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  msg1.textContent = "Loading...";
+  msg2.textContent = "";
+
+  const location = serach.value;
+
+  fetch("/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        msg1.textContent = data.error;
+      } else {
+        msg1.textContent = data.address;
+        msg2.textContent = data.forecast;
+      }
+    });
+  });
+});
